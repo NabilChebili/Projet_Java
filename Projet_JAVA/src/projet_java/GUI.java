7e298fd3d42cc8bc56babba1a30b2e3b050a0144
Pierre <pierre.woj@hotmail.fr>
2020-05-20 10:33:18 +0200

    LoginForm

--- Projet_JAVA/src/projet_java/GUI.java
+++ Projet_JAVA/src/projet_java/GUI.java
@@ -1,53 +1,71 @@
+package projet_java;
 
-import java.awt.*;
-import java.awt.event.*;
-import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JPasswordField;
+import javax.swing.JTextField;
 
-/**
- *
- * @author pierr
- */
-public class GUI implements ActionListener{
-    
-    int count = 0;
-    private JFrame frame;
-    private JLabel label;
-    private JPanel panel;
-    
-    public GUI() {
-        frame = new JFrame();
-        
-        JButton button = new JButton("Click me");
-        label = new JLabel("Number of clicks: 0");
-        button.addActionListener(this);
-        
-        panel = new JPanel();
-        
-        panel.setBorder(BorderFactory.createEmptyBorder(30, 30, 10, 30));
-        panel.setLayout(new GridLayout(0, 1));
-        panel.add(button);
-        panel.add(label);
-        
-        frame.add(panel, BorderLayout.CENTER);
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setTitle("Our Gui");
-        frame.pack();
-        frame.setVisible(true);
-    }
-    
+public class GUI implements ActionListener {
+
+    private static JLabel userLabel;
+    private static JTextField userText;
+    private static JLabel pwdLabel;
+    private static JPasswordField pwdText;
+    private static JButton button;
+    private static JLabel success;
+
     public static void main(String[] args) {
-        new GUI();
+        JFrame frame = new JFrame();
+        JPanel panel = new JPanel();
+        frame.setSize(350, 200);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setVisible(true);
+        frame.add(panel);
+
+        panel.setLayout(null);
+        userLabel = new JLabel("User");
+        userLabel.setBounds(10, 20, 80, 25);
+        panel.add(userLabel);
+
+        userText = new JTextField();
+        userText.setBounds(100, 20, 165, 25);
+        panel.add(userText);
+
+        pwdLabel = new JLabel("Password");
+        pwdLabel.setBounds(10, 50, 80, 25);
+        panel.add(pwdLabel);
+
+        pwdText = new JPasswordField();
+        pwdText.setBounds(100, 50, 165, 25);
+        panel.add(pwdText);
+
+        button = new JButton("Login");
+        button.setBounds(10, 80, 80, 25);
+        button.addActionListener(new GUI());
+        panel.add(button);
+
+        success = new JLabel("");
+        success.setBounds(10, 110, 300, 25);
+        panel.add(success);
     }
-    
+
     @Override
     public void actionPerformed(ActionEvent e) {
-        count++;
-        label.setText("Number of clicks: " + count);
+        String user = userText.getText();
+        String pwd = pwdText.getText();
+
+        System.out.println(user + " " + pwd);
+
+        if (user.equals("Pierre") && pwd.equals("password")) {
+            success.setText("Login successful");
+        }
+        else {
+            success.setText("Error");
+        }
     }
 }
